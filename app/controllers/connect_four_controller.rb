68888c3e223881aeab49c77362c47fdffcaaee9f
Buddy Williams <buddywilliams@gmail.com>
2018-02-11 15:54:39 -0500

    checkpoint: basic PvP working

--- app/controllers/connect_four_controller.rb
+++ app/controllers/connect_four_controller.rb
@@ -1,2 +1,25 @@
 class ConnectFourController < ApplicationController
+  def index
+  end
+
+  def move
+    width = params[:width]
+    height = params[:height]
+    game_mode = params[:game_mode]
+    moves = params[:moves]
+
+    b = Board.new(width, height)
+    b.auto_move(moves)
+
+    win_results = b.detect_win
+    game_over = (!win_results[0].nil? or b.tie?) ? true : false
+
+    render json: {
+      game_over: game_over,
+      moves: b.all_moves,
+      current_players_turn: b.turn,
+      coords: win_results[1],
+      winner: win_results[0]
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'connect_four#index'
+  post '/move', to: 'connect_four#move'
 end
